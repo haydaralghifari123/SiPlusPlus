92eae44657680e3bac88ade0e77ea82436949c1e
BimoJri <bimmjri@gmail.com>
2026-01-12 06:20:27 +0700

    Midtrans callback controller 1,1

--- app/Http/Controllers/MidtransCallbackController.php
+++ app/Http/Controllers/MidtransCallbackController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Transaction;
+use App\Models\Feature\Transaction;
 use App\Models\Feature\UserCourse;
 use Illuminate\Support\Facades\Log;
 
